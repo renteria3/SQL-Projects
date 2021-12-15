90de83d36164263d8822732e149fcd45e3a41a43
renteria3 <96163121+renteria3@users.noreply.github.com>
2021-12-14 23:24:04 -0600

    Update Project_Portfolio-COVID.sql

--- Project_Portfolio-COVID.sql
+++ Project_Portfolio-COVID.sql
@@ -127,34 +127,3 @@ CREATE VIEW PercentPopulationVaccinated AS
 	WHERE dea.continent IS NOT NULL
 
 SELECT * FROM PercentPopulationVaccinated
-
---Tableau Code
-
---1
-SELECT SUM(new_cases) AS Total_Cases, SUM(CAST(new_deaths AS INT)) AS Total_Deaths, 
-	SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS Death_Percent
-		FROM Portfolio_Project..CovidDeaths$
-	WHERE continent IS NOT NULL
-	ORDER BY 1,2
-
---2
-SELECT location, SUM(CAST(new_deaths AS INT)) AS Total_Death_Count
-		FROM Portfolio_Project..CovidDeaths$
-	WHERE continent IS NULL
-		AND location NOT IN ('World','European Union','International')
-	GROUP BY location
-	ORDER BY Total_Death_Count DESC
-
---3
-SELECT location, population, MAX(total_cases) AS Highest_Infection_Count, 
-	MAX((total_cases/population))*100 AS Percent_Population_Infected
-		FROM Portfolio_Project..CovidDeaths$
-	GROUP BY location, population
-	ORDER BY Percent_Population_Infected DESC
-
---4
-SELECT location, population, date, MAX(total_cases) AS Highest_Infection_Count,
-	MAX((total_cases/population))*100 AS Percent_Population_Infected
-		FROM Portfolio_Project..CovidDeaths$
-	GROUP BY location, population, date
-	ORDER BY Percent_Population_Infected DESC
